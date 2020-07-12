8edbd022cf3aff9cdb80d06d3eb512d7e5e7e236
Dren88 <38310704+Dren88@users.noreply.github.com>
2020-07-12 22:31:10 +0500

    Add files via upload

---
+++ mycomponents/news.list/.parameters.php
@@ -0,0 +1,64 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+/** @var array $arCurrentValues */
+
+if(!CModule::IncludeModule("iblock"))
+	return;
+
+$arTypesEx = CIBlockParameters::GetIBlockTypes(array("-"=>" "));
+
+
+$arProperty_LNS = array();
+$rsProp = CIBlockProperty::GetList(array("sort"=>"asc", "name"=>"asc"), array("ACTIVE"=>"Y", "IBLOCK_ID"=>(isset($arCurrentValues["IBLOCK_ID"])?$arCurrentValues["IBLOCK_ID"]:$arCurrentValues["ID"])));
+while ($arr=$rsProp->Fetch())
+{
+    $arProperty[$arr["CODE"]] = "[".$arr["CODE"]."] ".$arr["NAME"];
+    if (in_array($arr["PROPERTY_TYPE"], array("L", "N", "S")))
+    {
+        $arProperty_LNS[$arr["CODE"]] = "[".$arr["CODE"]."] ".$arr["NAME"];
+    }
+}
+
+$arIBlocks=array();
+$db_iblock = CIBlock::GetList(array("SORT"=>"ASC"), array("SITE_ID"=>$_REQUEST["site"], "TYPE" => ($arCurrentValues["IBLOCK_TYPE"]!="-"?$arCurrentValues["IBLOCK_TYPE"]:"")));
+while($arRes = $db_iblock->Fetch())
+	$arIBlocks[$arRes["ID"]] = "[".$arRes["ID"]."] ".$arRes["NAME"];
+$arComponentParameters = array(
+	"GROUPS" => array(
+	),
+	"PARAMETERS" => array(
+        "PROPERTY_CODE" => array(
+            "PARENT" => "DATA_SOURCE",
+            "NAME" => GetMessage("T_IBLOCK_PROPERTY"),
+            "TYPE" => "LIST",
+            "MULTIPLE" => "Y",
+            "VALUES" => $arProperty_LNS,
+            "ADDITIONAL_VALUES" => "Y",
+        ),
+		"IBLOCK_TYPE" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("T_IBLOCK_DESC_LIST_TYPE"),
+			"TYPE" => "LIST",
+			"VALUES" => $arTypesEx,
+			"DEFAULT" => "news",
+			"REFRESH" => "Y",
+		),
+		"IBLOCK_ID" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("T_IBLOCK_DESC_LIST_ID"),
+			"TYPE" => "LIST",
+			"VALUES" => $arIBlocks,
+			"DEFAULT" => '={$_REQUEST["ID"]}',
+			"ADDITIONAL_VALUES" => "Y",
+			"REFRESH" => "Y",
+		),
+		"NEWS_COUNT" => array(
+			"PARENT" => "BASE",
+			"NAME" => GetMessage("T_IBLOCK_DESC_LIST_CONT"),
+			"TYPE" => "STRING",
+			"DEFAULT" => "20",
+		),
+	),
+);
+
+
